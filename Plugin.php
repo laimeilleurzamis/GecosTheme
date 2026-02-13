234c1d964a4c25ab4232e6ced75058967deb5839
MaxenceRoques <maxence.roques@etu.univ-cotedazur.fr>
2026-02-13 13:51:19 +0100

    feat page createAlerte, dashboard and userPage

--- Plugin.php
+++ Plugin.php
@@ -6,13 +6,19 @@ use Kanboard\Core\Plugin\Base;
 
 class Plugin extends Base
 {
-public function initialize()
-{
-    $this->hook->on('template:layout:css', array('template' => 'plugins/GecosTheme/Assets/css/theme.css'));
-    $this->helper->register('gecos', '\Kanboard\Plugin\GecosTheme\Helper\GecosTheme');
+    public function initialize()
+    {
+        $this->hook->on('template:layout:css', array('template' => 'plugins/GecosTheme/Assets/css/theme.css'));
+        $this->hook->on('template:layout:css', array('template' => 'plugins/GecosTheme/Assets/css/userPage/userPage.css'));
+        $this->hook->on('template:layout:css', array('template' => 'plugins/GecosTheme/Assets/css/dashboardPage/dashboardPage.css'));
+        $this->hook->on('template:layout:css', array('template' => 'plugins/GecosTheme/Assets/css/dashboardPage/activityView.css'));
+        $this->hook->on('template:layout:css', array('template' => 'plugins/GecosTheme/Assets/css/createAlerte/createAlerte.css'));
 
-    $this->template->setTemplateOverride('project_header/search', 'GecosTheme:project_header/search');
-}
+
+        $this->helper->register('gecos', '\Kanboard\Plugin\GecosTheme\Helper\GecosTheme');
+
+        $this->template->setTemplateOverride('project_header/search', 'GecosTheme:project_header/search');
+    }
 
     public function getPluginName()
     {
